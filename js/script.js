5c426c3f14ed968843968a2ebd4059b2bf13b2f9
Ariel <arielladret@gmail.com>
2019-10-29 16:43:59 -0700

    HTML tidy

--- js/script.js
+++ js/script.js
@@ -8,37 +8,37 @@ $(function() {
 
   // select elements
   const dropdown = $("#select");
-  const storyContainer = $("#stories");
-  const logo = $(".logo");
 
   // on select
   dropdown.on("change", function(event) {
-    const category = event.target.value;
+    const category = $(this).val();
 
     // show loader
 
     // get stories
     $.ajax({
       method: "GET",
-      url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=L4iO7u1rIyh2nxWEMFiKU5SOX79LQHo8`
+      url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=L4iO7u1rIyh2nxWEMFiKU5SOX79LQHo8`,
+      dataType: "json"
     })
       .done(function(data) {
         // fill grid with stories
-        const articles = data.results;
-        console.log(articles);
+        const imageOnly = data.results.filter(function(items) {
+          if (items.multimedia[4] !== undefined) {
+            return true;
+          } else {
+            return false;
+          }
+        });
 
-        logo.addClass("move-up");
+        const topTwelve = imageOnly.slice(0, 12);
 
         // reveal grid with stories
-        $.each(articles, function(key, value) {
-          storyContainer.append(`
-            <li>
-              <a target="_blank" href="${value.url}">
-              <img src="${value.multimedia[4].url}"> 
-              <p>${value.abstract}</p>
-              </a>s
-            </li>
-          `);
+        $.each(topTwelve, function(key, value) {
+          console.log("this", value);
+          $(".top-stories").append(
+            `<article class="article" style="background-image:url(${value.multimedia[4].url}); background-size: cover;"><p>${value.abstract}</p></article>`
+          );
         });
       })
       .fail(function() {
