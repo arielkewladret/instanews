77df460dcafb98c8a3a685a48754e065868e7124
Ariel <arielladret@gmail.com>
2019-11-01 12:19:53 -0700

    mobile styling

--- js/script.js
+++ js/script.js
@@ -41,7 +41,8 @@ $(function() {
             <article class="article" style="background-image:url(${value.multimedia[4].url}); background-size: cover;"><p>${value.abstract}</p></a></article>`
           );
         });
-        // const moveUp = document.querySelector(".move-up");
+
+        $("#site-header").addClass("header-small");
 
         // moveUp.addEventListener("click", () => {
         //   header.classList.toggle("select");
