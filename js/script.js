7c047e71d452801c55195aa7ddd86f96de4cf016
Ariel <arielladret@gmail.com>
2019-11-04 09:32:35 -0800

    minor style adjustments

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 $(function() {
-  // event listeners + behaviours + loops/timeout
-
-  // select elements
   const dropdown = $("#select");
   const loader = $("#loader");
 
-  // on select
   dropdown.on("change", function() {
     const category = $(this).val();
 
-    // show loader
     loader.show();
 
-    // get stories
     $.ajax({
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=L4iO7u1rIyh2nxWEMFiKU5SOX79LQHo8`,
@@ -21,7 +15,6 @@ $(function() {
       .done(function(data) {
         $("#top-stories").empty();
 
-        // fill grid with stories
         const imageOnly = data.results.filter(function(items) {
           if (items.multimedia[4] !== undefined) {
             return true;
@@ -32,7 +25,6 @@ $(function() {
 
         const topTwelve = imageOnly.slice(0, 12);
 
-        // reveal grid with stories
         $.each(topTwelve, function(key, value) {
           const link = value.url;
           console.log("this", value);
@@ -50,7 +42,6 @@ $(function() {
       })
       .fail(function() {
         alert("Something went wrong");
-        // $(".user-name").append("Sorry there was an error");
       })
       .always(function() {
         loader.hide();
